b6c6c6d2ec05fbd96c2448ad45c4a2545ac15d3e
aletardio <ale.tardio99@gmail.com>
2023-12-20 15:48:03 +0100

    Added for to generate emails and ended exercise

--- js/script.js
+++ js/script.js
@@ -3,13 +3,19 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            
+            emails: []
         }
     },
     created() {
-        
+        this.getEmails();
     },
     methods: {
-        
-    },
+        getEmails(){
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+                    this.emails.push(response.data.response);
+                    });
+                }
+            }
+        }
 }).mount('#app')
